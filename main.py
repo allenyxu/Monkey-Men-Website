5acd30a8fc542904d4cfdef0ed42a31ce5774a0d
purplebears321 <purplebears321@gmail.com>
2020-10-22 20:19:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import projects
+# https://flask.palletsprojects.com/en/1.1.x/api/
+from flask import Flask, render_template
+#create a Flask instance
+app = Flask(__name__)
+
+#connects default URL of server to a python function
+@app.route('/')
+def home():
+    #function use Flask import (Jinga) to render an HTML template
+    return render_template("home.html")
+
+@app.route('/hello/')
+def hello_rooute():
+  return render_template("hello.html", projects=projects.setup())
+
+if __name__ == "__main__":
+    #runs the application on the repl development server
+    app.run(debug=True, port='3000', host='0.0.0.0')
+
