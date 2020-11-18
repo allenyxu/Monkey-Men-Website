f47e917d073db291cd132161798f21e02bf2ccc3
allenyxu <72947445+allenyxu@users.noreply.github.com>
2020-11-17 19:52:20 -0800

    Update main.py

--- main.py
+++ main.py
@@ -16,5 +16,5 @@ def hello_rooute():
 
 if __name__ == "__main__":
     #runs the application on the repl development server
-    app.run(debug=True, port='3000', host='0.0.0.0')
+    app.run(debug=True, port='5000', host='127.0.0.')
 
